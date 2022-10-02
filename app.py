26b38ebe96053a71d231dcc91e58e227027f4e4f
aokimirai <mirai_he.317@icloud.com>
2022-10-02 21:56:06 +0900

    csrf

--- app.py
+++ app.py
@@ -7,17 +7,29 @@ import werkzeug
 from datetime import datetime
 import sqlite3
 import re
+from flask_wtf.csrf import CSRFProtect
+
+csrf = CSRFProtect
 
 app = Flask(__name__, static_folder='upload')
+csrf.init_app(app)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # Configure session to use filesystem (instead of signed cookies)
 # ファイルシステムを使用するようにセッションを構成します (署名付き Cookie の代わりに)
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# csrf対策
+with open("securet.txt") as f:
+    securet = f.read()
+securetkey = securet
+app.config['WTF_CSRF_SECRET_KEY'] = securetkey
+app.config['WTF_CSRF_ENABLED'] = True
 
 # 画像のアップロード
 UPLOAD_POST_FOLDER = './upload'
